8e90ec11282b5d3a019e86cc9f545523bcfbce58
agp-id <59814882+agp-id@users.noreply.github.com>
2026-08-20 18:40:19 +0700

    Update MainActivity.java

--- app/src/main/java/com/uwuh/pif/MainActivity.java
+++ app/src/main/java/com/uwuh/pif/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.log
+import android.util.Log;  // ← Log dengan L BESAR
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
