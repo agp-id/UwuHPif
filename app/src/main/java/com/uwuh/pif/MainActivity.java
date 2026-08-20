2c1ed4b5e0eb78e77b45446a85e66f6f18f1dfee
agp-id <59814882+agp-id@users.noreply.github.com>
2026-08-20 18:37:53 +0700

    Update MainActivity.java

--- app/src/main/java/com/uwuh/pif/MainActivity.java
+++ app/src/main/java/com/uwuh/pif/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.log
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
