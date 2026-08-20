641c85bbb03f9cb7ebd691a5dfa75b2f4e123edb
agp-id <59814882+agp-id@users.noreply.github.com>
2026-08-20 18:17:02 +0700

    Create SystemPropertiesHelper.java

---
+++ app/src/main/java/com/uwuh/pif/SystemPropertiesHelper.java
@@ -0,0 +1,43 @@
+package com.uwuh.pif;
+
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+public class SystemPropertiesHelper {
+    private static final String TAG = "SysPropHelper";
+
+    public static String get(String key) {
+        return get(key, null);
+    }
+
+    public static String get(String key, String def) {
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class, String.class);
+            return (String) get.invoke(null, key, def);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
+    public static boolean getBoolean(String key, boolean def) {
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method getBoolean = c.getMethod("getBoolean", String.class, boolean.class);
+            return (boolean) getBoolean.invoke(null, key, def);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
+    public static void set(String key, String value) {
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method set = c.getMethod("set", String.class, String.class);
+            set.invoke(null, key, value);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set: " + key + "=" + value);
+        }
+    }
+}
