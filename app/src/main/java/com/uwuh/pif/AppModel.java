314243c3c6153c7d04f7eedd39bd23587d6164c5
agp-id <59814882+agp-id@users.noreply.github.com>
2026-08-21 20:10:51 +0700

    Create AppModel.java

---
+++ app/src/main/java/com/uwuh/pif/AppModel.java
@@ -0,0 +1,26 @@
+package com.uwuh.pif;
+
+import android.graphics.drawable.Drawable;
+
+public class AppModel {
+    private String appName;
+    private String packageName;
+    private Drawable icon;
+    private boolean isSystem;
+    private String selectedConfig;
+
+    public AppModel(String appName, String packageName, Drawable icon, boolean isSystem, String selectedConfig) {
+        this.appName = appName;
+        this.packageName = packageName;
+        this.icon = icon;
+        this.isSystem = isSystem;
+        this.selectedConfig = selectedConfig;
+    }
+
+    public String getAppName() { return appName; }
+    public String getPackageName() { return packageName; }
+    public Drawable getIcon() { return icon; }
+    public boolean isSystem() { return isSystem; }
+    public String getSelectedConfig() { return selectedConfig; }
+    public void setSelectedConfig(String selectedConfig) { this.selectedConfig = selectedConfig; }
+}
